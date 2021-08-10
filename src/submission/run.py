7bf0dde8b1b54febd5ac86b54bfc84ea08265c42
Akshay Agarwal <dl-ebay-pl-pd@ebay.com>
2021-08-10 19:25:27 -0400

    updated

--- src/submission/run.py
+++ src/submission/run.py
@@ -114,7 +114,7 @@ elif args.function == 'finetune':
     finetune_dataset = dataset.NameDataset(text_finetune, pretrain_dataset)
 
 
-    tconf = trainer.TrainerConfig(max_epochs=10, batch_size=512, learning_rate=6e-4,
+    tconf = trainer.TrainerConfig(max_epochs=10, batch_size=256, learning_rate=6e-4,
                       lr_decay=True, warmup_tokens=512*20, final_tokens=200*len(pretrain_dataset)*block_size,
                       num_workers=4, ckpt_path =args.writing_params_path )
     trainerModel = trainer.Trainer(model, finetune_dataset, None, tconf)
